4c0f9adb3063983bb26add998621f029e67747ea
Fábio Sebastian Silveira <sebas-sil@users.noreply.github.com>
2020-01-16 08:18:42 -0300

    Projeto react vazio

---
+++ frontend-web/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// componentes: função que retorna um JS, CSS ou HTML (colocar um componente por arquivo)
+// propriedade: são os parametros da função (atributos das tags)
+// estado: informação mantida pelo componente, gerenciado pelo userState (imutabilidade)
+
+function App() {
+  return (
+    <h1>Hello World</h1>
+  );
+}
+
+export default App;
