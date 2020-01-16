3bd5ce1154482298763eebf1cf135865e3b5314d
Fábio Sebastian Silveira <sebas-sil@users.noreply.github.com>
2020-01-16 10:32:38 -0300

    Adicionado CORS para acessar a plicação por ajax de dominuis diferentes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const mongoose= require('mongoose')
+const cors    = require('cors')
 const routes  = require('./routes')
-const app = express()
-const dotenv = require('dotenv')
+const app     = express()
+const dotenv  = require('dotenv')
+
 
 dotenv.config()
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0-9q0zd.mongodb.net/devs?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true, connectTimeoutMS: 1000})
 
+app.use(cors())
 // configura para todas as requisições que o express entenderá JSON
 app.use(express.json())
 app.use(routes)
